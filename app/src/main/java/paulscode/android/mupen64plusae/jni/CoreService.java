ddfa06b13391a4b44ec052a0c6d3e31e91d7d4b7
fzurita <fzurita@gmail.com>
2020-10-31 14:56:19 -0400

    Use crashlytics for error reporting

--- app/src/main/java/paulscode/android/mupen64plusae/jni/CoreService.java
+++ app/src/main/java/paulscode/android/mupen64plusae/jni/CoreService.java
@@ -59,6 +59,8 @@ import android.util.Log;
 import android.util.SparseArray;
 import android.view.Surface;
 
+import com.google.firebase.crashlytics.FirebaseCrashlytics;
+
 import org.mupen64plusae.v3.alpha.R;
 
 import java.io.BufferedReader;
@@ -602,6 +604,7 @@ public class CoreService extends Service implements CoreInterface.OnFpsChangedLi
                         ArrayList<CoreTypes.m64p_cheat_code> cheats = getCheat(cheatText, selection.getOption());
                         if (!cheats.isEmpty()) {
                             mCoreInterface.coreAddCheat(cheatText.name, cheats);
+                            FirebaseCrashlytics.getInstance().setCustomKey("Cheat" + selection.getIndex(), cheatText.name);
                         }
                     }
                 }
