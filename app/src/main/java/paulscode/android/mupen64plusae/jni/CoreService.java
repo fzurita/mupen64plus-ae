8226b859ecb183ab60038843f4160bacf9da791e
fzurita <fzurita@gmail.com>
2020-11-24 13:50:54 -0500

    Much improved Google drive support
    Allow uploading and downloading individually for each game
    Don't disable google drive options when support is disabled
    Allow singing out of Google Drive

--- app/src/main/java/paulscode/android/mupen64plusae/jni/CoreService.java
+++ app/src/main/java/paulscode/android/mupen64plusae/jni/CoreService.java
@@ -675,7 +675,9 @@ public class CoreService extends Service implements CoreInterface.OnFpsChangedLi
 
 
             // Sync to Google Drive
-            SyncToGoogleDriveService.syncToGoogleDrive(getApplicationContext(), mGamePrefs.getGameDataDirName(), mRomGoodName, mRomHeaderName);
+            if (mGlobalPrefs.backupToGoogleDrive) {
+                SyncToGoogleDriveService.syncToGoogleDrive(getApplicationContext(), mGamePrefs.getGameDataDirName(), mRomGoodName, mRomHeaderName, false);
+            }
 
             mCoreInterface.closeRom();
             mCoreInterface.emuShutdown();
